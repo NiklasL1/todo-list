4a2aa7da6f0e239194dce6a07fbbf6b9064a3039
Niklas Little <niklas@tomlittle.org>
2020-06-17 16:40:55 +0200

    trying to implement marking an item as done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 let editTitle = document.getElementById('listTitle');
-editTitle.ondblclick = function(){
+editTitle.ondblclick = function () {
   this.removeAttribute('readonly');
 };
 
@@ -11,17 +11,27 @@ addItemButton.addEventListener("click", () => {
     alert("Cannot add empty item!");
   }
   else {
-    document.getElementById("insertItem").insertAdjacentHTML("afterbegin", `<li class="container row">    
-    <div class="col-2 bg-light border d-flex align-middle justify-content-center">
-    <i class="p-2 fa fa-check-square" aria-hidden="true"></i>
-    <i class="p-2 fa fa-trash" aria-hidden="true"></i>
-    <i class="p-2 fa fa-pencil" aria-hidden="true"></i>
-    </div>
-    <div class="col-10 bg-light border align-middle">
-    ${addItemInput.value}
-    </div>
-    </li>`); 
+    document.getElementById("insertItem").insertAdjacentHTML("afterbegin",
+      `<li class="container row">    
+        <div class="col-2 bg-light border d-flex align-middle justify-content-center">
+          <i class="p-2 fa fa-check-square markAsDone" aria-hidden="true"></i>
+          <i class="p-2 fa fa-trash" aria-hidden="true"></i>
+          <i class="p-2 fa fa-pencil" aria-hidden="true"></i>
+        </div>
+        <div class="col-10 bg-light border align-middle todoItem">
+        ${addItemInput.value}
+        </div>
+      </li>`);
 
-    document.getElementById('addItemInput').value = "";    
-  }      
+    document.getElementById('addItemInput').value = "";
+  }
 });
+
+let markAsDone = document.querySelector(".markAsDone")
+
+if (markAsDone) {
+  markAsDone.addEventListener("click", () => {
+    alert("test");
+  });
+
+}
