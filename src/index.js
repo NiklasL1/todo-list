4fdf08d0a8e2a3930b876d7742c9bb8fa97a897d
Niklas Little <niklas@tomlittle.org>
2020-06-22 17:18:07 +0200

    making the icons in new rows work with their own nodes

--- src/index.js
+++ src/index.js
@@ -33,26 +33,39 @@ class todoList {
   }
   else {
     const insertItem = document.getElementById("insertItem");
-    const deleteTodo = document.querySelector(".deleteTodo");
-    let node = document.createElement("LI");        
-    node.insertAdjacentHTML("afterbegin",
-      `<div class= "container row">
-      <div class="col-2 bg-light border d-flex align-middle justify-content-center">
-          <i class="p-2 fa fa-check-square markAsDone" aria-hidden="true"></i>
-          <i class="p-2 fa fa-trash deleteTodo" aria-hidden="true"></i>
-          <i class="p-2 fa fa-pencil" aria-hidden="true"></i>
+            
+    insertItem.insertAdjacentHTML("afterbegin",
+      `<li class= "container row">
+      <div class="col-2 bg-light border d-flex align-middle justify-content-center">   
         </div>
         <div class="col-10 bg-light border align-middle todoItem">
           ${addItemInput.value}
         </div>
-        </div>`);    
-    insertItem.appendChild(node);
-
-    deleteTodo.addEventListener("click", () => {
-      alert("test");
-    
-    console.log(deleteTodo);
+        </li>`); 
+        
+    let node = document.createElement("SPAN");    
+    node.addEventListener("click", () => {
+      alert("trash");      
     });
+    node.insertAdjacentHTML("afterbegin",
+    `<i class="p-2 fa fa-trash" aria-hidden="true"></i>`);      
+    document.querySelector(".col-2").appendChild(node);
+    
+    let node2 = document.createElement("SPAN");    
+    node2.addEventListener("click", () => {
+      alert("check");      
+    });
+    node2.insertAdjacentHTML("afterbegin",
+    `<i class="p-2 fa fa-check-square" aria-hidden="true"></i>`);      
+    document.querySelector(".col-2").appendChild(node2);
+    
+    let node3 = document.createElement("SPAN");    
+    node3.addEventListener("click", () => {
+      alert("edit");      
+    });
+    node3.insertAdjacentHTML("afterbegin",
+    `<i class="p-2 fa fa-pencil" aria-hidden="true"></i>`);      
+    document.querySelector(".col-2").appendChild(node3); 
 
     document.getElementById('addItemInput').value = "";
   }
