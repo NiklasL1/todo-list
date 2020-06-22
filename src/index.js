a7be1df339c8dc68f2d424fefe0186333e6e2584
Niklas Little <niklas@tomlittle.org>
2020-06-22 16:35:46 +0200

    addding classes and trying to make icons work in added rows

--- src/index.js
+++ src/index.js
@@ -3,35 +3,69 @@ editTitle.ondblclick = function () {
   this.removeAttribute('readonly');
 };
 
-let addItemButton = document.getElementById('addItemButton');
-let addItemInput = document.getElementById('addItemInput');
+const addItemButton = document.getElementById('addItemButton');
 
-addItemButton.addEventListener("click", () => {
+addItemButton.addEventListener("click", () => {  
+  const addItemInput = document.getElementById('addItemInput');
+  let newTodo = new todoItem(addItemInput);
+  TodoList1.addTodo(newTodo);
+  console.log(TodoList1);
+});
+
+class todoItem {
+  constructor (content, index) {
+    this.content = content;
+    this.index = TodoList1.todoArray[index];
+    this.highPriority = false;
+  }
+};
+
+class todoList {
+  constructor (title, todoArray = []) {
+    this.todoArray = todoArray;
+    this.title = title;
+  }
+
+  addTodo(content) {
+  this.todoArray.unshift(new todoItem(content));
   if (addItemInput.value === "") {
     alert("Cannot add empty item!");
   }
   else {
-    document.getElementById("insertItem").insertAdjacentHTML("afterbegin",
-      `<li class="container row">    
-        <div class="col-2 bg-light border d-flex align-middle justify-content-center">
+    const insertItem = document.getElementById("insertItem");
+    const deleteTodo = document.querySelector(".deleteTodo");
+    let node = document.createElement("LI");        
+    node.insertAdjacentHTML("afterbegin",
+      `<div class= "container row">
+      <div class="col-2 bg-light border d-flex align-middle justify-content-center">
           <i class="p-2 fa fa-check-square markAsDone" aria-hidden="true"></i>
-          <i class="p-2 fa fa-trash" aria-hidden="true"></i>
+          <i class="p-2 fa fa-trash deleteTodo" aria-hidden="true"></i>
           <i class="p-2 fa fa-pencil" aria-hidden="true"></i>
         </div>
         <div class="col-10 bg-light border align-middle todoItem">
-        ${addItemInput.value}
+          ${addItemInput.value}
         </div>
-      </li>`);
+        </div>`);    
+    insertItem.appendChild(node);
+
+    deleteTodo.addEventListener("click", () => {
+      alert("test");
+    
+    console.log(deleteTodo);
+    });
 
     document.getElementById('addItemInput').value = "";
   }
-});
+  }
 
-let markAsDone = document.querySelector(".markAsDone")
+  deleteTodo(index) {
+  this.todoArray.splice(index, 1)
+  }
 
-if (markAsDone) {
-  markAsDone.addEventListener("click", () => {
-    alert("test");
-  });
+  editTodo(index, content) {  
+  this.todoArray[index] = content
+  }
+  
+};
 
-}
+let TodoList1 = new todoList("TodoList1")
